6960c9e999f271091a83e92644f2058848c9983e
Jong-Han <jonghan9611@gmail.com>
2022-03-19 21:22:44 +0900

    2초 후 자동검색 기능 구현

--- app/src/main/java/com/jh/kakaoimagesearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/jh/kakaoimagesearch/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jh.kakaoimagesearch.ui.main
 
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.jh.kakaoimagesearch.BR
@@ -7,7 +8,10 @@ import com.jh.kakaoimagesearch.R
 import com.jh.kakaoimagesearch.base.BaseActivity
 import com.jh.kakaoimagesearch.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -21,12 +25,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     private val mainAdapter by lazy { MainAdapter() }
 
+    @OptIn(FlowPreview::class)
     override fun initViewAndEvent() {
 
         dataBinding.rvImage.adapter = mainAdapter
 
         lifecycleScope.launch {
-            viewModel.getSearchResult().collectLatest {
+            viewModel.searchString.debounce(2000)
+                .filter {
+                    it.isNotEmpty()
+                }.collect {
+                    Toast.makeText(this@MainActivity, "검색을 시작합니다.", Toast.LENGTH_SHORT).show()
+                    mainAdapter.refresh()
+                    viewModel.getSearchResult(it)
+                }
+        }
+
+        lifecycleScope.launch {
+            viewModel.pagingData.collect {
                 mainAdapter.submitData(it)
             }
         }
--- app/src/main/java/com/jh/kakaoimagesearch/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jh/kakaoimagesearch/ui/main/MainViewModel.kt
@@ -1,13 +1,34 @@
 package com.jh.kakaoimagesearch.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.jh.kakaoimagesearch.data.remote.response.Document
+import com.jh.kakaoimagesearch.di.IoDispatcher
 import com.jh.kakaoimagesearch.usecase.SearchUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val searchUseCase: SearchUseCase): ViewModel() {
+class MainViewModel @Inject constructor(private val searchUseCase: SearchUseCase,
+                                        @IoDispatcher private val ioDispatcher: CoroutineDispatcher): ViewModel() {
 
-    fun getSearchResult() = searchUseCase.getSearchResult("apple")
+    val searchString = MutableStateFlow("")
+
+    private val _pagingData = MutableStateFlow<PagingData<Document>>(PagingData.empty())
+    val pagingData: StateFlow<PagingData<Document>> = _pagingData
+
+    fun getSearchResult(searchString: String) {
+        viewModelScope.launch(ioDispatcher) {
+            searchUseCase.getSearchResult(searchString).collectLatest {
+                _pagingData.value = it
+            }
+        }
+    }
 
 }
